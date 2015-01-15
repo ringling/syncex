d966d6bc6b370bb78489a7eb4b132026bc9b2dea
Thomas Ringling <thomas.ringling@gmail.com>
2015-01-15 20:17:03 +0100

    Add file logging

--- config/config.exs
+++ config/config.exs
@@ -1,19 +1,18 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for third-
-# party users, it should be done in your mix.exs file.
+config :logger,
+  backends: [{LoggerFileBackend, :error_log}, :console],
+  level: :debug,
+  format: "$time $metadata[$level] $message\n"
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
+config :logger, :error_log,
+  level: :warn,
+  path: "logs/error.log",
+  format: "$date $time [$level] $metadata$message\n"
+
+config :logger, :console,
+  level: :debug,
+  format: "$date $time [$level] $metadata$message\n"
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
@@ -22,3 +21,4 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
