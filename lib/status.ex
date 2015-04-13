b4081f53b7a4ea5c55bd014e2477b2daa3fbe630
ringling <thomas.ringling@gmail.com>
2015-04-13 11:13:14 +0200

    Convert Status module into GenServer

--- lib/status.ex
+++ lib/status.ex
@@ -1,11 +1,44 @@
 defmodule Syncex.Status do
+  use GenServer
+  require Logger
+
+  ## Client API
+
+  @name {:global,__MODULE__}
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, name: @name)
+  end
 
   def in_sync?(service \\ %{sequence_server: Syncex.Sequence.Server, location_service: LocationService}) do
-    current_seq(service.sequence_server) == max_seq(service.location_service)
+    GenServer.call(@name, {:in_sync?, service})
+  end
+
+  def latest_synced_event(update_worker \\ Syncex.UpdateWorker) do
+    GenServer.call(@name, {:latest_synced_event, update_worker})
+  end
+
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    Logger.info "Status started"
+    {:ok, []}
+  end
+
+  def handle_call({:in_sync?, service}, _from, state) do
+    reply = current_seq(service.sequence_server) == max_seq(service.location_service)
+    {:reply, reply, state}
+  end
+
+
+  def handle_call({:latest_synced_event, update_worker}, _from, state) do
+    {:reply, update_worker.latest_synced_event, state}
   end
 
   defp current_seq(sequence_server), do: sequence_server.get_sequence
 
   defp max_seq(location_service), do: location_service.max_sequence_number
+
 
 end
--- test/status_test.exs
+++ test/status_test.exs
@@ -1,21 +1,46 @@
 defmodule Syncex.StatusTest do
   use ExUnit.Case, async: true
 
-  defmodule SequenceServerStub do
-    def get_sequence, do: 1
+
+  setup do
+    {:ok, status_server} = Syncex.Status.start_link
+    {:ok, status_server: status_server}
   end
 
-  defmodule LocationServiceStub do
+  defmodule SequenceServerStub do
+    def get_sequence, do: 99
+  end
+
+  defmodule LocationServiceNotInSyncStub do
     def max_sequence_number, do: 1
+  end
+
+  defmodule LocationServiceInSyncStub do
+    def max_sequence_number, do: 99
+  end
+
+  defmodule UpdateWorkerStub do
+    def latest_synced_event, do: :event
   end
 
   test "is in sync" do
     services = %{
       sequence_server: SequenceServerStub,
-      location_service: LocationServiceStub
+      location_service: LocationServiceInSyncStub
     }
-    
     assert Syncex.Status.in_sync?(services)
+  end
+
+  test "is not in sync" do
+    services = %{
+      sequence_server: SequenceServerStub,
+      location_service: LocationServiceNotInSyncStub
+    }
+    refute Syncex.Status.in_sync?(services)
+  end
+
+  test "latest synced event" do
+    assert Syncex.Status.latest_synced_event(UpdateWorkerStub) == :event
   end
 
 end
