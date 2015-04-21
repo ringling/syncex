aaaa3a6e0a4efad34eaa31544b57d143c7c2dcb7
ringling <thomas.ringling@gmail.com>
2015-04-21 13:47:34 +0200

    Map category to type

--- lib/location_sanitizer.ex
+++ lib/location_sanitizer.ex
@@ -4,10 +4,12 @@ defmodule Syncex.LocationSanitizer do
     Removes/replaces legacy and unclean location data
   """
   def sanitize(location) do
-    postal_code = location["postal_code"]
-      |> String.strip
-      |> adjust_postal_code
-    location |> Map.put("postal_code",postal_code)
+    location |> sanitize_postal_code |> map_category_to_type
+  end
+
+  defp sanitize_postal_code(location) do
+    postal_code = location["postal_code"] |> String.strip |> adjust_postal_code
+    location |> Map.put("postal_code", postal_code)
   end
 
   defp adjust_postal_code("1404"), do: "1460"
@@ -20,5 +22,15 @@ defmodule Syncex.LocationSanitizer do
   defp adjust_postal_code("1162"), do: "1159"
   defp adjust_postal_code("1007"), do: "1070"
   defp adjust_postal_code(postal_code), do: postal_code
+
+  defp map_category_to_type(location), do: map_category_to_type(has_type?(location), location)
+
+  defp map_category_to_type(true, location), do: location
+  defp map_category_to_type(false, location) do
+    type = location |> Map.get("category", "user")
+    location |> Map.put("type", type)
+  end
+
+  defp has_type?(location), do: Map.has_key?(location, "type")
 
 end
